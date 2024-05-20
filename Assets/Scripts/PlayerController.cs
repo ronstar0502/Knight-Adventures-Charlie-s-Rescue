35fd0d57d61f8a04926a6d11a20682c5bae82026
Ron Star <ronstar0502@gmail.com>
2024-05-20 20:38:21 +0300

    ..

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D rb;
+    private float horizantalInput, jumpInput;
+    private bool onGround;
+    [SerializeField] private float moveSpeed, jumpForce;
+    [SerializeField] private float damping = 0.25f;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        horizantalInput = Input.GetAxis("Horizantal");
+        jumpInput = Input.GetAxis("Jump");
     }
 }
--- Assets/Scripts/UI/QuitButton.cs
+++ Assets/Scripts/UI/QuitButton.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuitButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button bt;
+
+    private void Start()
     {
-        
+        bt = GetComponent<Button>();
+        bt.onClick.AddListener(QuitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
