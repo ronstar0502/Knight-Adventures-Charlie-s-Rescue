f5a1aa9b73c3c0e62fcfa524942d10425ee820cc
Ron Star <ronstar0502@gmail.com>
2024-06-26 17:33:21 +0300

    level changes , and movement changes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,15 +6,21 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed, jumpForce;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
     [SerializeField] private Transform groundCheck;
-    [SerializeField] private float checkRadius;
+    [SerializeField] private float checkRadius = 0.2f;
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float accelerationTime = 0.1f;
+    [SerializeField] private float decelerationTime = 0.1f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+    [SerializeField] private float lowJumpMultiplier = 2f;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Animator animator;
     private float horizontalInput;
     private bool onFloor;
+    private float velocitySmoothing;
 
     private void Start()
     {
@@ -25,23 +31,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        /*horizontalInput = Input.GetAxis("Horizontal");
-        if(onFloor && horizontalInput != 0)
-        {
-            rb.velocity = new Vector2(horizontalInput*moveSpeed,rb.velocity.y);
-        }
-        if (jumpInput != 0 && onFloor)
-        {
-            Jump();
-        }*/
-
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        onFloor = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         FlipPlayerDirection();
+
         if (Input.GetKeyDown(KeyCode.Space) && onFloor)
         {
             Jump();
         }
-    }
 
+        ApplyFallMultiplier();
+    } 
 
     private void FixedUpdate()
     {
@@ -50,29 +50,38 @@ public class PlayerController : MonoBehaviour
 
     private void Movement()
     {
-        onFloor = Physics2D.OverlapCircle(groundCheck.position,checkRadius,whatIsGround);
-        horizontalInput = Input.GetAxis("Horizontal");
+        float movementSpeed = horizontalInput * moveSpeed;
+        float smoothedMovementSpeed = Mathf.SmoothDamp(rb.velocity.x, movementSpeed, ref velocitySmoothing, horizontalInput != 0 ? accelerationTime : decelerationTime);
+        //ref velocitySmoothing ->  velocitySmoothing is used by Mathf.SmoothDamp to keep track of the velocity change over time
+        //horizontalInput != 0 ? accelerationTime : decelerationTime -> a way to put a value with a statement
+        rb.velocity = new Vector2(smoothedMovementSpeed, rb.velocity.y);
 
-        if (horizontalInput != 0)
-        {
-            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
-            animator.SetBool("isRunning", true);
-        }
-        else animator.SetBool("isRunning", false);
+        animator.SetBool("isRunning", horizontalInput != 0); // another way to set the bool
     }
 
+    private void ApplyFallMultiplier()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
     private void Jump()
-    {       
-        rb.velocity = Vector2.up * jumpForce;
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
 
     private void FlipPlayerDirection()
     {
-        if (horizontalInput == 1)
+        if (horizontalInput > 0)
         {
             sr.flipX = false;
         }
-        else if (horizontalInput == -1)
+        else if (horizontalInput < 0)
         {
             sr.flipX = true;
         }
